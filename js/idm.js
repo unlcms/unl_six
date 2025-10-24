a5d57ed42ca349f7844d0e983ff278d62a664f20
Eric Rasmussen <ericrasmussen1@gmail.com>
2025-10-24 13:26:54 -0500

    Make the user's name available in JS for use with UNL Login widget in idm.js

--- js/idm.js
+++ js/idm.js
@@ -1,4 +1,7 @@
 (function (drupalSettings) {
   window.UNL.idm.pushConfig('loginRoute', drupalSettings.path.baseUrl + 'caslogin?destination=' + drupalSettings.path.currentPath);
   window.UNL.idm.pushConfig('logoutRoute', drupalSettings.path.baseUrl + 'user/logout');
+  if (drupalSettings.user.name) {
+    window.UNL.idm.pushConfig('serverUser', drupalSettings.user.name);
+  }
 })(drupalSettings);
