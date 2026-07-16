81282edf87b53b0ed56cd5c7b6647142728de601
Eric Rasmussen <ericrasmussen1@gmail.com>
2026-07-15 19:20:17 -0500

    Populate from the parent first

--- src/Element/WebformMultiple.php
+++ src/Element/WebformMultiple.php
@@ -4,7 +4,6 @@ namespace Drupal\unl_six\Element;
 
 use Drupal\webform\Element\WebformMultiple as WebformMultipleContrib;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\webform\Utility\WebformElementHelper;
 
 /**
  * Provides a webform element to assist in creation of multiple elements.
@@ -17,6 +16,8 @@ class WebformMultiple extends WebformMultipleContrib {
    * Process items and build multiple elements widget.
    */
   public static function processWebformMultiple(&$element, FormStateInterface $form_state, &$complete_form) {
+    $element = parent::processWebformMultiple($element, $form_state, $complete_form);
+
     // Add DCF classes to "add more items" button for webform_mutiple element type.
     $element['add']['submit']['#attributes']['class'][] = 'dcf-btn';
     $element['add']['submit']['#attributes']['class'][] = 'dcf-btn-secondary';
